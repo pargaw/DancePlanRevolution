fa29091d92d04b8d7af906c2b099110c1513df8f
Remi M <mirr3000@gmail.com>
2017-04-15 14:41:10 -0400

    Enabled cdetection of currentTask number

--- js/task.js
+++ js/task.js
@@ -5,3 +5,21 @@ var TASKS = ["attendance", "videos", "announcements"]
 var currentTask; // should be 0, 1, or 2, specifying one of the above tasks
 
 // if we change currentTask, we change the content displayed by the carousel in task.html
+
+$(document).on('click', '.tasks a', function(e) {
+	currentTask = $(this).attr('id');
+	console.log(currentTask);
+});
+
+
+// top bar
+var taskCarousel = $('.taskCarousel');
+if (currentTask == 0) {
+	pass;
+} else if (currentTask == 1) {
+	pass;
+} else if (currentTask == 2) {
+	pass;
+}
+
+// main content
