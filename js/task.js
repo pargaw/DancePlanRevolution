fc18f7d17d74ff6c302538c1c1914966c26ea2f0
Peniel Argaw <peniel@mit.edu>
2017-04-16 21:41:22 -0400

    updated organization

--- js/task.js
+++ js/task.js
@@ -8,6 +8,56 @@ document.head.appendChild(imported);
 var TASKS = ["attendance", "videos", "announcements"]
 var currentTask = 0; // should be 0, 1, or 2, specifying one of the above tasks
 
+var taskCarousel = $('.carousel');
+
+// if we change currentTask, we change the content displayed by the carousel in task.html
+$(document).on('click', '.tasks a', function(e) {
+	currentTask = $(this).attr('id');
+	updateTaskContent();
+});
+
+$(document).on('click', '#addNew', function(e) {
+	addButtonsUpdate();
+});
+
+function updateTaskContent() {
+	var newTaskButton = $('.new_task');
+	if (currentTask == 0) { 
+		newTaskButton.find('p').text('Add new member for today');
+	} else if (currentTask == 1) {
+		newTaskButton.find('p').text('Upload new video');
+	} else if (currentTask == 2) {
+		newTaskButton.find('p').text('Add new announcement');
+	}	
+}
+
+// Attendance
+// var members = ['beth','rob','stef','david']
+// var table = $('#myTable');
+// console.log($(this));
+// var row = table.insertRow(0);
+// members.forEach(function(value){
+//   console.log(members);
+//   var cell1 = row.insertCell(0);
+//   var cell2 = row.insertCell(1);
+//   cell1.innerHTML = "hi";
+// });
+
+//Videos
+function addButtonsUpdate(){
+	var newTaskButton = $('.new_task');
+	if (currentTask == 0) { 
+	} else if (currentTask == 1) {
+		var myDropzone = new Dropzone(document.body, 
+			{ url: "/file/post"});
+		myDropzone.on('addedfile', function(file){
+			file.previewElement.querySelector().onclick;
+		})
+	} else if (currentTask == 2) {
+	} 
+}
+
+//Announcements
 var ANNOUNCEMENTS = {
 	"0" : {
 		"date": "1980-01-02T00:00:00.000Z", 
@@ -29,40 +79,4 @@ function createNewAnnouncement() {
 	para.appendChild(node);
 	var element = document.getElementById("div1");
 	element.appendChild(para);
-}
-
-// if we change currentTask, we change the content displayed by the carousel in task.html
-$(document).on('click', '.tasks a', function(e) {
-	currentTask = $(this).attr('id');
-	updateTaskContent();
-});
-
-$(document).on('click', '#addNew', function(e) {
-	addButtonsUpdate();
-});
-
-function updateTaskContent() {
-	var taskCarousel = $('.carousel');
-	var newTaskButton = $('.new_task');
-	if (currentTask == 0) { 
-		newTaskButton.find('p').text('Add new member for today');
-	} else if (currentTask == 1) {
-		newTaskButton.find('p').text('Upload new video');
-	} else if (currentTask == 2) {
-		newTaskButton.find('p').text('Add new announcement');
-	}	
-}
-
-function addButtonsUpdate(){
-	var taskCarousel = $('.carousel');
-	var newTaskButton = $('.new_task');
-	if (currentTask == 0) { 
-	} else if (currentTask == 1) {
-		var myDropzone = new Dropzone(document.body, 
-			{ url: "/file/post"});
-		myDropzone.on('addedfile', function(file){
-			file.previewElement.querySelector().onclick;
-		})
-	} else if (currentTask == 2) {
-	} 
 }
